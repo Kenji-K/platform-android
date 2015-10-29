e5fc5a88f5b6a98ebee9c79b67923a563b2f45e5
Henry Addo <addhen@linuxmail.org>
2015-10-29 18:00:38 +0900

    Implement custom view pager to add support for enabling and disabling scroll

---
+++ ushahidi/src/main/java/com/ushahidi/android/presentation/view/ui/widget/ScrollConfigurableViewPager.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2015 Ushahidi.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program in the file LICENSE-AGPL. If not, see
+ *  https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+package com.ushahidi.android.presentation.view.ui.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Custom view pager to enable the ability to enable and disable scroll on the view pager
+ *
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public class ScrollConfigurableViewPager extends ViewPager {
+
+    private boolean mEnabled;
+
+    public ScrollConfigurableViewPager(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        if (mEnabled) {
+            return super.onTouchEvent(motionEvent);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent motionEvent) {
+        if (mEnabled) {
+            return super.onInterceptTouchEvent(motionEvent);
+        }
+        return false;
+    }
+
+    public void enableScroll(boolean enabled) {
+        mEnabled = enabled;
+    }
+}
